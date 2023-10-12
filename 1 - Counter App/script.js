e916af8385333bedd24eac5326b62b0fa71bd1c8
Renan Beraldi Castiglioni <renanberaldi@outlook.com>
2023-10-12 08:44:23 -0300

    1 - Counter App

--- 1 - Counter App/script.js
+++ 1 - Counter App/script.js
@@ -1,2 +1,32 @@
 "use strict";
 
+const count = document.querySelector(".count");
+const buttons = document.querySelector(".buttons");
+
+buttons.addEventListener("click", (e) => {
+  if (e.target.classList.contains("add")) {
+    count.innerHTML++;
+    setColor();
+  }
+
+  if (e.target.classList.contains("subtract")) {
+    count.innerHTML--;
+    setColor();
+  }
+
+  if (e.target.classList.contains("reset")) {
+    count.innerHTML = 0;
+    setColor();
+  }
+});
+
+const setColor = () => {
+  const colorChoosed =
+    count.innerHTML < 0
+      ? (count.style.color = "orangered")
+      : count.innerHTML > 0
+      ? (count.style.color = "yellow")
+      : (count.style.color = "#fff");
+
+  return colorChoosed;
+};
